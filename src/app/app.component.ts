01531e1404d1dc0a3e71cec0fda3791604f1937f
Blessing <bntoko@gmail.com>
2020-11-09 13:18:57 +0200

    working on primitives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as THREE from 'three';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'threePrimitives';
+export class AppComponent implements OnInit {
+  public scene = new THREE.Scene();
+  public camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, .1, 1000);
+  public renderer = new THREE.WebGLRenderer({
+    antialias: true
+  });
+
+
+  ngOnInit() {
+    this.init();
+  }
+
+  private init() {
+    this.scene.background = new THREE.Color(0xaaaaaa);
+    this.camera.position.z = 120;
+  }
 }
